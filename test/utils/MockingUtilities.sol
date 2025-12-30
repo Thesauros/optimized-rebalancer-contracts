ff94e38a25b6f8d8a437eba67b5f775fda826691
avelevon <avelevon23@gmail.com>
2025-12-30 22:21:46 +0400

    update tests

--- test/utils/MockingUtilities.sol
+++ test/utils/MockingUtilities.sol
@@ -47,7 +47,6 @@ contract MockingUtilities is Test {
     bytes32 public constant ADMIN_ROLE = 0x00;
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
     bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
-    bytes32 public constant DEPOSITOR_ROLE = keccak256("DEPOSITOR_ROLE");
 
     constructor() {
         asset = new MockERC20("Test USDT", "tUSDT", ASSET_DECIMALS);
