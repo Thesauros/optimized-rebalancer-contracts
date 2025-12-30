33f2d6eec765d59f478d64859dcfcd09a897bc0b
avelevon <avelevon23@gmail.com>
2025-12-30 20:33:59 +0400

    update utils

--- utils/move-blocks.ts
+++ utils/move-blocks.ts
@@ -1,9 +1,9 @@
-import { network } from "hardhat";
+import { network } from 'hardhat';
 
 export async function moveBlocks(amount: number) {
   for (let index = 0; index < amount; index++) {
     await network.provider.request({
-      method: "evm_mine",
+      method: 'evm_mine',
       params: [],
     });
   }
